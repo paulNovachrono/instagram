e8ae921dfc37df9df44853931b513bf49e56ec20
Debajoyti Paul <learn.ishan2001@gmail.com>
2026-03-16 23:12:28 +0530

    Bcrypt js has been added

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,6 +1,7 @@
 const userModel = require("../model/user.model");
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const registerController = async (req, res) => {
   const { userName, name, email, bio, profileImage, password } = req.body;
@@ -19,10 +20,7 @@ const registerController = async (req, res) => {
           : "username Already Exists"),
     });
 
-  const hashedPassword = crypto
-    .createHash("sha256", process.env.CRYPTO_SECRET)
-    .update(password)
-    .digest("hex");
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
     const registeredUser = await userModel.create({
@@ -72,19 +70,14 @@ const loginController = async (req, res) => {
     return;
   }
 
-  const hashedPassword = crypto
-    .createHash("sha256", process.env.CRYPTO_SECRET)
-    .update(password)
-    .digest("hex");
-
-  const passwordMatched = isUserExists.password === hashedPassword;
+  const passwordMatched = await bcrypt.compare(password, isUserExists.password);
 
   if (!passwordMatched) {
     res.status(409).json({ message: "Invalid Password" });
     return;
   }
 
-  const token = jwt.sign({ id: passwordMatched._id }, process.env.JWT_SECRET, {
+  const token = jwt.sign({ id: isUserExists._id }, process.env.JWT_SECRET, {
     expiresIn: "1d",
   });
   res.cookie("token", token, {
@@ -94,7 +87,7 @@ const loginController = async (req, res) => {
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
-  res.status(201).json({
+  res.status(200).json({
     message: "User LoggedIn Successfully",
     user: {
       email: isUserExists.email,
