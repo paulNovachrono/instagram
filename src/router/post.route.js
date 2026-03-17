43b496da9e57090ee6414d41462375505a2f5e5b
Debajoyti Paul <learn.ishan2001@gmail.com>
2026-03-17 16:42:14 +0530

    A helpful comment for understanding multer

--- src/router/post.route.js
+++ src/router/post.route.js
@@ -4,6 +4,8 @@ const upload = multer({ storage: multer.memoryStorage() });
 const { createPostController } = require("../controller/post.controller");
 const postRouter = express.Router();
 
+// in the middlewhere [upload.single('from-input-name')] should be there
+
 postRouter.post("/", upload.single("imgUrl"), createPostController);
 
 module.exports = postRouter;
